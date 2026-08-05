e3ecd37a371ec4b929d4e125ed01485fcf550726
Tsaishashanth <saishashanthtarigopula@gmail.com>
2026-08-05 06:45:04 +0530

    rewind techstack

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,20 +1,20 @@
 export default function TechStack() {
   const technologies = [
-    "React.js",
-    "Next.js",
-    "Express.js",
-    "Golang",
-    "Hono",
-    "Node.js",
-    "AWS",
-    "JavaScript",
-    "TypeScript",
-    "Prisma",
-    "MongoDB",
-    "PostgreSQL",
-    "Docker",
-    "Tailwind CSS",
-    "Redis",
+    "⚛️ React.js",
+    "▲ Next.js",
+    "◇ Express.js",
+    "🔥 Golang",
+    "🟠 Hono",
+    "🟢 Node.js",
+    "☁️ AWS",
+    "JS JavaScript",
+    "TS TypeScript",
+    "▲ Prisma",
+    "🍃 MongoDB",
+    "🐘 PostgreSQL",
+    "🐳 Docker",
+    "〰 Tailwind CSS",
+    "🔴 Redis",
   ];
 
   return (
@@ -25,7 +25,21 @@ export default function TechStack() {
         {technologies.map((tech) => (
           <div
             key={tech}
-            className="rounded-xl border border-zinc-700 bg-zinc-900 px-4 py-2 text-sm font-medium text-white"
+            className="
+              flex items-center gap-2
+              rounded-lg
+              border border-dashed border-zinc-700
+              bg-zinc-700/40
+              px-4 py-2
+              text-sm
+              font-semibold
+              text-zinc-100
+              backdrop-blur-sm
+              shadow-sm
+              transition
+              hover:border-zinc-500
+              hover:bg-zinc-800
+            "
           >
             {tech}
           </div>
