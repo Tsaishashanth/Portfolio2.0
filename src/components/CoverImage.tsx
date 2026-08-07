cfaa6a004ac7aaef7846fd821cf6238b5280cfb6
Tsaishashanth <saishashanthtarigopula@gmail.com>
2026-08-07 19:33:46 +0530

    rewind the header photo

--- src/components/CoverImage.tsx
+++ src/components/CoverImage.tsx
@@ -2,13 +2,15 @@ import Image from 'next/image';
 
 export default function CoverImage() {
   return (
-    <div className="h-64 w-full overflow-hidden">
+    <div className="relative w-full aspect-[3/1]">
       <Image
         src="/header mountain for portfolio1.png"
         alt="Portfolio cover header"
-        width={1200}
-        height={400}
+        fill
+        //width={1200}
+        //height={400}
         className="h-full w-full object-cover"
+        priority
       />
     </div>
   );
